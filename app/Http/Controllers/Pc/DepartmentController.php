e16fb03ef5d4d30a534213215000e3384c9cc2e4
xzjianglong <littledragoner@163.com>
2022-04-09 16:59:19 +0800

    feat: 删除单位提示信息

--- app/Http/Controllers/Pc/DepartmentController.php
+++ app/Http/Controllers/Pc/DepartmentController.php
@@ -64,8 +64,6 @@ class DepartmentController extends Controller
             $department->delete();
         }
 
-
-
         return $departmentNamesHasUser
             ? error(implode(',', $departmentNamesHasUser) . "或其下级部门已关联人员，请先解除关联", Response::HTTP_UNPROCESSABLE_ENTITY)
             : no_content();
