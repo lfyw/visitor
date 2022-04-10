614f2fa3f13e8ade1b00ef1c226b2a9b93b774b8
xzjianglong <littledragoner@163.com>
2022-04-10 18:26:00 +0800

    feat: 审核情况查询增加访问日期筛选

--- app/Http/Controllers/Api/AuditController.php
+++ app/Http/Controllers/Api/AuditController.php
@@ -9,31 +9,31 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\AuditRequest;
 use App\Http\Resources\Api\AuditResource;
 use App\Models\Audit;
-use App\Models\Auditor;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\QueryException;
 use Illuminate\Http\Response;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\DB;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AuditController extends Controller
 {
     public function index()
     {
-        return AuditResource::collection(Audit::whereIdCard(\request('id_card'))->with([
-            'user:id,name,department_id',
-            'user.department.ancestors',
-            'ways',
-            'visitorType',
-            'auditors.user:id,name',
-        ])->latest()->paginate(\request('pageSize', 10)));
+        return AuditResource::collection(Audit::accessDateFrom(request('access_date_from'))
+            ->accessDateTo(request('access_date_to'))
+            ->whereIdCard(\request('id_card'))
+            ->with([
+                'user:id,name,department_id',
+                'user.department.ancestors',
+                'ways',
+                'visitorType',
+                'auditors.user:id,name',
+            ])->latest()->paginate(\request('pageSize', 10)));
     }
 
     public function store(AuditRequest $auditRequest)
     {
-        $audit = DB::transaction(function () use ($auditRequest){
+        $audit = DB::transaction(function () use ($auditRequest) {
             $validated = Arr::except($auditRequest->validated(), ['face_picture_ids', 'way_ids']);
             $validated['gender'] = InfoHelper::identityCard()->sex($auditRequest->id_card) == 'M' ? '男' : '女';
             $validated['age'] = InfoHelper::identityCard()->age($auditRequest->id_card);
@@ -43,16 +43,16 @@ class AuditController extends Controller
             $audit->attachFiles($auditRequest->face_picture_ids);
 
             // 添加审批人，根据配置文件寻找
-            $visitorSetting = $audit->visitorType->visitorSettings()->whereHas('ways',fn(Builder $builder) => $builder->whereIn('id', $auditRequest->way_ids))->first();
+            $visitorSetting = $audit->visitorType->visitorSettings()->whereHas('ways', fn(Builder $builder) => $builder->whereIn('id', $auditRequest->way_ids))->first();
             throw_unless($visitorSetting, new MissingVisitorSettingException('请先在后台添加对应的访客设置', Response::HTTP_NOT_FOUND));
 
-            collect($visitorSetting->approver)->sortBy('order')->each(function ($approver) use ($audit){
-                if ($approver['type'] == ApproverType::INTERVIEWEE->getValue()){
+            collect($visitorSetting->approver)->sortBy('order')->each(function ($approver) use ($audit) {
+                if ($approver['type'] == ApproverType::INTERVIEWEE->getValue()) {
                     $audit->auditors()->create([
                         'user_id' => $audit->user_id,
                     ]);
                 }
-                if ($approver['type'] == ApproverType::ROLE->getValue()){
+                if ($approver['type'] == ApproverType::ROLE->getValue()) {
                     $roler = User::whereRoleId($approver['role_id'])->first();
                     $audit->auditors()->create([
                         'user_id' => $roler->id,
