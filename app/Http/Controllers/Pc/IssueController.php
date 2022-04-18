c44988e47617a87365ef79b4cb92a61edb3f9e4a
xzjianglong <littledragoner@163.com>
2022-04-18 14:33:12 +0800

    feat: 增加人员和访客全部下发

--- app/Http/Controllers/Pc/IssueController.php
+++ app/Http/Controllers/Pc/IssueController.php
@@ -7,6 +7,8 @@ use App\Http\Resources\Pc\IssueResource;
 use App\Jobs\PushUser;
 use App\Jobs\PushVisitor;
 use App\Models\Issue;
+use App\Models\User;
+use App\Models\Visitor;
 use App\Supports\Sdks\VisitorIssue;
 use Illuminate\Http\Response;
 
@@ -86,6 +88,44 @@ class IssueController extends Controller
             'limiter' => '访问次数限制'
         ]);
         foreach (request('id_cards') as $idCard){
+            PushUser::dispatch(
+                $idCard,
+                request('access_date_from'),
+                request('access_date_to'),
+                request('access_time_from'),
+                request('access_time_to'),
+                request('limiter')
+            );
+        }
+        return send_message('后台下发中...', Response::HTTP_OK);
+    }
+
+    public function allVisitor()
+    {
+        $visitors = Visitor::all();
+        foreach ($visitors->pluck('id_card')->toArray() as $idCard){
+            PushVisitor::dispatch($idCard);
+        }
+        return send_message('后台下发中...', Response::HTTP_OK);
+    }
+
+    public function allUser()
+    {
+        $this->validate(request(), [
+            'access_date_from' => ['required'],
+            'access_date_to' => ['required'],
+            'access_time_from' => ['required'],
+            'access_time_to' => ['required'],
+            'limiter' => ['required'],
+        ], [], [
+            'access_date_from' => '起始访问日期',
+            'access_date_to' => '截止访问日期',
+            'access_time_from' => '起始访问时间',
+            'access_time_to' => '截止访问时间',
+            'limiter' => '访问次数限制'
+        ]);
+        $users = User::all();
+        foreach ($users->pluck('id_card')->toArray() as $idCard){
             PushUser::dispatch(
                 $idCard,
                 request('access_date_from'),
