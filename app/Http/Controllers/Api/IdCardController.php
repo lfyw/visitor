a7f19f5ac1b1699494b30f3771fca97e2b4044b5
xzjianglong <littledragoner@163.com>
2022-05-07 09:35:06 +0800

    perf: 年龄超过60岁不允许登记

--- app/Http/Controllers/Api/IdCardController.php
+++ app/Http/Controllers/Api/IdCardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
+use AlicFeng\IdentityCard\Application\IdentityCard;
 use AlicFeng\IdentityCard\InfoHelper;
 use App\Http\Controllers\Controller;
 use App\Models\Blacklist;
@@ -16,6 +17,9 @@ class IdCardController extends Controller
         }
         if (!InfoHelper::identityCard()->validate(\request('id_card', ''))){
             return error('不是合法的身份证号', Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        if ((new IdentityCard())->age(request('id_card')) > 60){
+            return error('年龄超过60岁不允许登记', Response::HTTP_UNPROCESSABLE_ENTITY);
         }
         if (Blacklist::idCard(\request('id_card'))->exists()){
             return error('您已被纳入黑名单，无法进行访客登记', Response::HTTP_UNPROCESSABLE_ENTITY);
