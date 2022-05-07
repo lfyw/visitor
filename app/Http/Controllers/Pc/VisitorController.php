0dc722a79622edcc1577b6f45c3931dc4461034f
xzjianglong <littledragoner@163.com>
2022-05-07 16:04:07 +0800

    fix:

--- app/Http/Controllers/Pc/VisitorController.php
+++ app/Http/Controllers/Pc/VisitorController.php
@@ -103,7 +103,7 @@ class VisitorController extends Controller
                 $visitor->access_date_from,
                 $visitor->access_date_to,
                 $visitor->access_time_from,
-                $visitor->access_time_to.
+                $visitor->access_time_to,
                 $visitor->limiter,
                 $visitor->ways
             )->onQueue('issue');
